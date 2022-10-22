c8fd97a24f557c0b92c463201dd573dfda736fd5
Pedro Figueiredo <pedrofigueiredo9@gmail.com>
2022-10-22 11:03:46 -0300

    Added Google maps to the project.

--- react-frontend/src/components/Mapping.tsx
+++ react-frontend/src/components/Mapping.tsx
@@ -1,15 +1,19 @@
 // @flow
 // generated with 'fsc' using reactjs code snippets extension
 import { Button, Grid, MenuItem, Select } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Loader } from "google-maps";
+import { useEffect, useRef, useState } from "react";
+import { getCurrentPosition } from "../util/geolocation";
 import { Route } from "../util/models";
 
 const API_URL = process.env.REACT_APP_API_URL;
+const googleMapsLoader = new Loader(process.env.REACT_APP_GOOGLE_API_KEY);
 
 type Props = {};
 export const Mapping = (props: Props) => {
   const [routes, setRoutes] = useState<Route[]>([]);
   const [routeIdSelected, setRouteIdSelected] = useState<string>("");
+  const mapRef = useRef<google.maps.Map>();
 
   useEffect(() => {
     fetch(`${API_URL}/routes`)
@@ -17,8 +21,24 @@ export const Mapping = (props: Props) => {
       .then((data) => setRoutes(data));
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      const [, position] = await Promise.all([
+        googleMapsLoader.load(),
+        getCurrentPosition({ enableHighAccuracy: true }),
+      ]);
+      const divMap = document.getElementById("map") as HTMLElement;
+      mapRef.current = new google.maps.Map(divMap, {
+        zoom: 15,
+        center: position,
+      });
+    })();
+  }, []);
+
+  //const startRoute = useCallback()
+
   return (
-    <Grid container>
+    <Grid container style={{ width: "100%", height: "100%" }}>
       <Grid item xs={12} sm={3}>
         <form>
           <Select
@@ -42,7 +62,7 @@ export const Mapping = (props: Props) => {
         </form>
       </Grid>
       <Grid item xs={12} sm={9}>
-        <div id="map"></div>
+        <div id="map" style={{ width: "100%", height: "100%" }}></div>
       </Grid>
     </Grid>
   );
