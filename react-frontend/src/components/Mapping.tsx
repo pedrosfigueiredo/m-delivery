64a7290f40508502d37be1a43ab6822422eb75d3
Pedro Figueiredo <pedrofigueiredo9@gmail.com>
2022-10-22 10:02:03 -0300

    App skeleton without Maps yet.

---
+++ react-frontend/src/components/Mapping.tsx
@@ -0,0 +1,49 @@
+// @flow
+// generated with 'fsc' using reactjs code snippets extension
+import { Button, Grid, MenuItem, Select } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Route } from "../util/models";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+type Props = {};
+export const Mapping = (props: Props) => {
+  const [routes, setRoutes] = useState<Route[]>([]);
+  const [routeIdSelected, setRouteIdSelected] = useState<string>("");
+
+  useEffect(() => {
+    fetch(`${API_URL}/routes`)
+      .then((data) => data.json())
+      .then((data) => setRoutes(data));
+  }, []);
+
+  return (
+    <Grid container>
+      <Grid item xs={12} sm={3}>
+        <form>
+          <Select
+            fullWidth
+            displayEmpty
+            value={routeIdSelected}
+            onChange={(event) => setRouteIdSelected(event.target.value + "")}
+          >
+            <MenuItem value="">
+              <em>Selecione uma corrida</em>
+            </MenuItem>
+            {routes.map((route, key) => (
+              <MenuItem key={key} value={route._id}>
+                <em>{route.title}</em>
+              </MenuItem>
+            ))}
+          </Select>
+          <Button type="submit" color="primary" variant="contained">
+            Iniciar uma corrida
+          </Button>
+        </form>
+      </Grid>
+      <Grid item xs={12} sm={9}>
+        <div id="map"></div>
+      </Grid>
+    </Grid>
+  );
+};
---
+++ react-frontend/src/theme.ts
@@ -0,0 +1,19 @@
+
+import { createTheme } from "@mui/material/styles";
+import { PaletteOptions } from "@mui/material/styles/createPalette";
+
+const palette: PaletteOptions = {
+  primary: {
+    main: "#FFCD00",
+    contrastText: "#242526",
+  },
+  background: {
+    default: "#242526",
+  },
+};
+
+const theme = createTheme({
+  palette,
+});
+
+export default theme;
